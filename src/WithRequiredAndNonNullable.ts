275af19ccd81e739c76dfd9a11edde43d8116c5b
Jason Williscroft <me@karmanivero.us>
2024-10-29 18:25:54 +0800

    rationalized external documentation

--- src/WithRequiredAndNonNullable.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Returns an object type with specific properties rendered required and non-nullable.
- *
- * @typeParam T - The object type to modify.
- * @typeParam K - Union of keys of `T` to render required and non-nullable.
- */
-export type WithRequiredAndNonNullable<T, K extends keyof T> = T & {
-  [P in K]-?: NonNullable<T[P]>;
-};
--- src/index.ts
+++ src/index.ts
@@ -16,26 +16,9 @@ export type {
   Unwrap,
 } from './Config';
 export { EntityManager } from './EntityManager';
-export type { Logger, LoggerEndpoint, LoggerOptions } from './Logger';
 export type { ParsedConfig } from './ParsedConfig';
 export type { QueryOptions } from './QueryOptions';
 export type { QueryResult } from './QueryResult';
 export type { ShardQueryFunction } from './ShardQueryFunction';
 export type { ShardQueryMap } from './ShardQueryMap';
 export type { ShardQueryResult } from './ShardQueryResult';
-export type { WithRequiredAndNonNullable } from './WithRequiredAndNonNullable';
-export {
-  type DefaultTranscodeMap,
-  defaultTranscodes,
-  type Entity,
-  type Exactify,
-  isNil,
-  type Nil,
-  type PartialTranscodable,
-  type PropertiesNotOfType,
-  type PropertiesOfType,
-  type SortOrder,
-  type TranscodableProperties,
-  type TranscodeMap,
-  type Transcodes,
-} from '@karmaniverous/entity-tools';
